dbec59645df8af79ffeff3c5bad61a162e8b5235
Mike Dark <agyemankojo47@gmail.com>
2024-01-05 12:03:22 +0000

    Create Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+export const Navbar = () => {
+ return (
+  <>
+   <div className="NavContainer flex justify-center gap-14 py-6 px-4 border-t border-b border-gray-300">
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Hotel</span>
+     <span className="material-symbols-outlined text-gray-600">bed</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Appartment</span>
+     <span className="material-symbols-outlined text-gray-600">apartment</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Villa</span>
+     <span className="material-symbols-outlined text-gray-600">villa</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Mansion</span>
+     <span className="material-symbols-outlined text-gray-600">castle</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Cabin</span>
+     <span className="material-symbols-outlined text-gray-600">cabin</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Tiny House</span>
+     <span className="material-symbols-outlined text-gray-600">house</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Camp House</span>
+     <span className="material-symbols-outlined text-gray-600">home_app_logo</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Trailer</span>
+     <span className="material-symbols-outlined text-gray-600">rv_hookup</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Tree House</span>
+     <span className="material-symbols-outlined text-gray-600">rv_hookup</span>
+    </div>
+    <div className="flex flex-col items-center">
+     <span className="text-gray-500">Beach House</span>
+     <span className="material-symbols-outlined text-gray-600">hot_tub</span>
+    </div>
+   </div>
+  </>
+ );
+};
