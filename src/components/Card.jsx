18041e8704a0dbf94a0b60dc4da8f02541c0e6aa
Mike Dark <agyemankojo47@gmail.com>
2024-01-05 14:35:41 +0000

    Create tabs

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+
+const Card = () => {
+  return (
+    <>
+    <section className="CardWrapper">Card
+    <div className="Cards">
+      <img src="" alt="" />
+
+      
+
+    </div>
+    </section>
+    
+    </>
+  )
+}
+
+export default Card
