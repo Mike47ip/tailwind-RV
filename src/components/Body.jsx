66e92dee31f1a1476fec2ad5854607f20cbc42ac
Mike Dark <agyemankojo47@gmail.com>
2024-01-08 12:00:58 +0000

    Customize scrollbar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,40 +1,46 @@
+import Card from "./Card";
+
 const Body = () => {
  return (
   <>
-   <div className="TabsWrapper flex justify-between items-center py-7 text-xs bg-red-50 bg-opacity-5">
-    <div className="Tab1 flex gap-8 px-3  list-none">
-     <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      All
-     </li>
+   <section className=" bg-red-50">
+    <div className="TabsWrapper flex justify-between items-center py-7 text-xs sm:overflow-x-auto sm:whitespace-nowrap">
+     <div className="Tab1 flex gap-8 px-3  list-none">
+      <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       All
+      </li>
 
-     <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      Top Villa
-     </li>
-     <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      Free Reschedule
-     </li>
-     <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      Book Now, Pay Later
-     </li>
-     <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      Self CheckIn
-     </li>
-     <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      Instant Book
-     </li>
+      <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       Top Villa
+      </li>
+      <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       Free Reschedule
+      </li>
+      <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       Book Now, Pay Later
+      </li>
+      <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       Self CheckIn
+      </li>
+      <li className="bg-white rounded-3xl border border-gray-300 px-4 py-2 font-inherit font-medium cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       Instant Book
+      </li>
+     </div>
+
+     <div className="Tab2 flex gap-8 px-3 list-none">
+      <li className="flex justify-center items-center font-inherit font-medium border border-gray-300 rounded-3xl bg-white px-4 py-1 cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       Filter
+       <span className="material-symbols-outlined text-base">filter_alt</span>
+      </li>
+      <li className="flex justify-center items-center font-inherit font-medium border border-gray-300 rounded-3xl bg-white px-4 py-1 cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
+       <span className="text-gray-500 px-inherit">Sort by:</span> Highest Price
+       <span className="material-symbols-outlined">expand_more</span>
+      </li>
+     </div>
     </div>
 
-    <div className="Tab2 flex gap-8 px-3 list-none">
-     <li className="flex justify-center items-center font-inherit font-medium border border-gray-300 rounded-3xl bg-white px-4 py-1 cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      Filter
-      <span className="material-symbols-outlined text-base">filter_alt</span>
-     </li>
-     <li className="flex justify-center items-center font-inherit font-medium border border-gray-300 rounded-3xl bg-white px-4 py-1 cursor-pointer transition transform hover:bg-red-100 hover:border-red-500 hover:-translate-y-0.5 active:bg-red-400 focus:bg-red-500">
-      <span className="text-gray-500 px-inherit">Sort by:</span> Highest Price
-      <span className="material-symbols-outlined">expand_more</span>
-     </li>
-    </div>
-   </div>
+    <Card />
+   </section>
   </>
  );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,332 @@
+import appartment1 from "../assets/images/appartment1.jpg";
+import appartment2 from "../assets/images/appartment2.jpg";
+import appartment3 from "../assets/images/appartment3.jpg";
+import appartment4 from "../assets/images/appartment4.jpg";
+import appartment5 from "../assets/images/appartment5.jpg";
+import appartment6 from "../assets/images/appartment6.jpg";
+import appartment7 from "../assets/images/appartment7.jpg";
+import appartment8 from "../assets/images/appartment8.jpg";
 
 const Card = () => {
-  return (
-    <>
-    <section className="CardWrapper">Card
+ return (
+  <>
+   <section className="CardWrapper grid gap-6 grid-cols-4 px-6">
     <div className="Cards">
-      <img src="" alt="" />
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment1}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
 
-      
-
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
     </div>
-    </section>
-    
-    </>
-  )
-}
+    <div className="Cards">
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment2}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
 
-export default Card
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
+    </div>
+    <div className="Cards">
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment3}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
+
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
+    </div>
+    <div className="Cards">
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment4}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
+
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
+    </div>
+    <div className="Cards">
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment5}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
+
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
+    </div>
+     <div className="Cards">
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment6}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
+
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
+    </div>
+     <div className="Cards">
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment7}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
+
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
+    </div>
+     <div className="Cards">
+     <img
+      className="object-cover h-54 rounded-xl"
+      src={appartment8}
+      alt="Appartments"
+     />
+     <ul className="ImageTab flex gap-2 list-none text-sm mt-3">
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Instant Book
+      </li>
+      <li className="bg-gray-200 px-2 py-1 rounded-2xl text-xs">
+       Selft Checkin
+      </li>
+     </ul>
+     <div className="flex justify-between items-center px-2">
+      <p className="HouseName text-lg pt-2 uppercase font-semibold">
+       House Name
+      </p>
+      <span className="text-yellow-500 flex items-center">
+       <ion-icon name="star"></ion-icon>
+       <span className="text-black">4.23</span>
+      </span>
+     </div>
+     <p className="text-xs px-2 text-gray-600">Location: India</p>
+
+     <div className="flex justify-between mt-6 items-center px-2">
+      <span className="roomSpec flex justify-center items-center p-1 border border-gray-300 rounded-2xl gap-1">
+       <ion-icon
+        className="material-symbols-outlined text-6xl"
+        name="bed"
+       ></ion-icon>
+       3<span className="material-symbols-outlined text-xl">bathtub</span>2
+       <ion-icon name="people"></ion-icon>3-4
+      </span>
+      <p className="prices font-semibold">
+       $2.000 <span className="text-gray-500">/n</span>
+      </p>
+     </div>
+    </div>
+
+   </section>
+  </>
+ );
+};
+
+export default Card;
